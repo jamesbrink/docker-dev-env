aa682fe97fcdfbd11db2d4d7eeb71d5f7d21dc3c
James Brink <brink.james@gmail.com>
2015-05-25 04:24:51 -0700

    updated shell.sh to copy in dotfiles

--- assets/bin/shell.sh
+++ assets/bin/shell.sh
@@ -7,6 +7,7 @@ if [ -a /runSetup ]; then
   echo "Creating user $USER_NAME"
   useradd -g users -s /bin/bash -d /home/$USER_NAME $USER_NAME
   echo "Setting permissions on /home/$USER_NAME"
+  cp -r /home/$USER_NAME/projects/dotfiles/{.bash_profile,.vimrc,.git,.gradle,.tmux.conf} /home/$USER_NAME/
   chown -R $USER_NAME:users /home/$USER_NAME
   su -c /local/opt/docker-assets/bin/setup-git.sh james
 fi
